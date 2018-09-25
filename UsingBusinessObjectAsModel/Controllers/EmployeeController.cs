7548b2e1cad94005c53c6deeba11d379226fcb0a
Baldev <Baldev@DESKTOP-TGB9VTP>
2018-09-25 01:15:45 -0500

    FormCollection, Saving From Data to DB.

--- UsingBusinessObjectAsModel/Controllers/EmployeeController.cs
+++ UsingBusinessObjectAsModel/Controllers/EmployeeController.cs
@@ -18,5 +18,27 @@ namespace UsingBusinessObjectAsModel.Controllers
             List<Employee> employees = employeeBusinessLayer.Employees.ToList();
             return View(employees);
         }
+        [HttpGet]
+        public ActionResult Create()
+        {
+          return View();
+        }
+        [HttpPost]
+        public ActionResult Create(FormCollection formCollection)
+        {
+            Employee employee = new Employee();
+            // Retrieve form data using form collection
+            employee.Name = formCollection["Name"];
+            employee.Gender = formCollection["Gender"];
+            employee.City = formCollection["City"];
+            employee.DateOfBirth =
+                Convert.ToDateTime(formCollection["DateOfBirth"]);
+
+            EmployeeBusinessLayer employeeBusinessLayer =
+                new EmployeeBusinessLayer();
+
+            employeeBusinessLayer.AddEmmployee(employee);
+            return RedirectToAction("Index");
+        }
     }
 }
