22938fef175fcaa3c55fa6907e2646f940296b3d
Baldev <Baldev@DESKTOP-TGB9VTP>
2018-09-24 19:47:00 -0500

    Added a Employee.cs model class and implemented complete model view controller functioning.

---
+++ WebApp1MVC/Controllers/EmployeeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApp1MVC.Models;
+
+namespace WebApp1MVC.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Details()
+        {
+            Employee employee = new Employee()
+            {
+
+                EmployeeId = 101,
+                Name = "John",
+                Gender = "Male",
+                City = "London"
+            };
+
+            
+            return View(employee);
+        }
+    }
+}
