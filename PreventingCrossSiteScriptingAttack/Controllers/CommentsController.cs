6d024ef5f93b7611aa9290d3f23142eb13f974fa
Baldev <Baldev@DESKTOP-TGB9VTP>
2018-09-29 14:19:47 -0500

    Preventing Cross Site Scripting Attack.

---
+++ PreventingCrossSiteScriptingAttack/Controllers/CommentsController.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PreventingCrossSiteScriptingAttack.Models;
+using System.Text;
+
+namespace PreventingCrossSiteScriptingAttack.Controllers
+{
+    public class CommentsController : Controller
+    {
+        private SampleDBEntities db = new SampleDBEntities();
+
+        // GET: Comments
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Comments.ToListAsync());
+        }
+
+        // GET: Comments/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Comment comment = await db.Comments.FindAsync(id);
+            if (comment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(comment);
+        }
+
+        // GET: Comments/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Comments/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ValidateInput(false)]
+        public async Task<ActionResult> Create([Bind(Include = "Id,Name,Comments")] Comment comment)
+        {
+            StringBuilder sbComments = new StringBuilder();
+
+            // Encode the text that is coming from comments textbox
+            sbComments.Append(HttpUtility.HtmlEncode(comment.Comments));
+
+            // Only decode bold and underline tags
+            sbComments.Replace("&lt;b&gt;", "<b>");
+            sbComments.Replace("&lt;/b&gt;", "</b>");
+            sbComments.Replace("&lt;u&gt;", "<u>");
+            sbComments.Replace("&lt;/u&gt;", "</u>");
+            comment.Comments = sbComments.ToString();
+
+            // HTML encode the text that is coming from name textbox
+            string strEncodedName = HttpUtility.HtmlEncode(comment.Name);
+            comment.Name = strEncodedName;
+            if (ModelState.IsValid)
+            {
+                db.Comments.Add(comment);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(comment);
+        }
+
+        // GET: Comments/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Comment comment = await db.Comments.FindAsync(id);
+            if (comment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(comment);
+        }
+
+        // POST: Comments/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Name,Comments")] Comment comment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(comment).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(comment);
+        }
+
+        // GET: Comments/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Comment comment = await db.Comments.FindAsync(id);
+            if (comment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(comment);
+        }
+
+        // POST: Comments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Comment comment = await db.Comments.FindAsync(id);
+            db.Comments.Remove(comment);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
