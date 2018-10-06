949d17e09791716356c33c1b3c1d0ea3eda9c08a
Baldev <Baldev@DESKTOP-TGB9VTP>
2018-10-06 00:00:54 -0500

    validation summary

--- Sortby/Models/Employee2.cs
+++ Sortby/Models/Employee2.cs
@@ -24,7 +24,7 @@ namespace Sortby.Models
         [DateRange("01/01/2000")]
         [DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true)]
         public Nullable<System.DateTime> HireDate { get; set; }
-        [RegularExpression(@"^(([A-za-z]+[\s]{1}[A-za-z]+)|([A-Za-z]+))$")]
+        [RegularExpression(@"^[\w-\._\+%]+@(?:[\w-]+\.)+[\w]{2,6}$", ErrorMessage = "Please enter a valid email address")]
         public string EmailAddress { get; set; }
         public Nullable<int> Salary { get; set; }
         public string PersonalWebSite { get; set; }
